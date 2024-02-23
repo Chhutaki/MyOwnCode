77cfb6d0dbf5cd18b941becece552303e3c271fa
shantosh <shantosh25@hotmail.com>
2024-02-23 08:21:32 +0530

    made code re factoring by adding before and after method

--- src/test/java/com/ws/tests/TestLoginPage.java
+++ src/test/java/com/ws/tests/TestLoginPage.java
@@ -1,6 +1,8 @@
 package com.ws.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.ws.base.DriverScript;
 import com.ws.pages.HomePage;
@@ -8,21 +10,33 @@ import com.ws.pages.LoginPage;
 
 public class TestLoginPage extends DriverScript {
 	
-	@Test(priority = 1)
-	public void testHomePageTitle() {
+	HomePage homePage;
+	LoginPage loginPage;
+	
+	@BeforeMethod
+	public void setUp()
+	{
 		initApplication();
-		HomePage homePage = new HomePage();
-		String title = homePage.getHomePageTitle();
-		Assert.assertTrue(title.contains("Demo Web Shop"));
+		homePage = new HomePage();
+		loginPage = new LoginPage();
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
 		quitDriver();
+	}
+	
+	@Test(priority = 1)
+	public void testHomePageTitle() 
+	{
+		String title = homePage.getHomePageTitle();
+		Assert.assertTrue(title.contains("Demo Web Shop"));	
 	}
 	
 	@Test(priority = 2)
 	public void testLoginFunction() {
-		initApplication();
-		HomePage homePage =new HomePage();
 		homePage.clickLogin();
-		LoginPage loginPage = new LoginPage();
 		String loginPageTitle = loginPage.getLoginPageTitle();
 		Assert.assertTrue(loginPageTitle.contains("Login"));
 		loginPage.enterEmail("selauto1@test.com");
@@ -31,25 +45,18 @@ public class TestLoginPage extends DriverScript {
 		String title = homePage.getHomePageTitle();
 		Assert.assertTrue(title.contains("Demo Web Shop"));
 		homePage.clickLogout();
-		quitDriver();
 	}
 	
 	@Test(priority = 3)
-	public void testIsForgotpasswordLinkPresent() {
-		initApplication();
-		HomePage homePage =new HomePage();
+	public void testIsForgotpasswordLinkPresent() 
+	{
 		homePage.clickLogin();
-		LoginPage loginPage = new LoginPage();
 		String loginPageTitle = loginPage.getLoginPageTitle();
 		Assert.assertTrue(loginPageTitle.contains("Login"));
 		boolean flag = loginPage.isForgotpasswordLinkPresent();
 		Assert.assertTrue(flag);
-		homePage.clickLogout();
-		quitDriver();
 	}
 
 }
-
-
 
 
