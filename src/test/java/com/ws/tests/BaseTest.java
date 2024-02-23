94a5ff9157335dcf4b51c259d30c84f650e59f25
shantosh <shantosh25@hotmail.com>
2024-02-23 08:26:33 +0530

    added basetest class

---
+++ src/test/java/com/ws/tests/BaseTest.java
@@ -0,0 +1,28 @@
+package com.ws.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import com.ws.base.DriverScript;
+import com.ws.pages.HomePage;
+import com.ws.pages.LoginPage;
+
+public class BaseTest extends DriverScript {
+	
+	HomePage homePage;
+	LoginPage loginPage;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		initApplication();
+		homePage = new HomePage();
+		loginPage = new LoginPage();
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		quitDriver();
+	}
+
+}
