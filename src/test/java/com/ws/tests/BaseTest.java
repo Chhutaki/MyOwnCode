af692dc2a2e977c877284b7704f5ac6e5a58e050
shantosh <shantosh25@hotmail.com>
2024-02-27 07:59:27 +0530

    added code for extent report

--- src/test/java/com/ws/tests/BaseTest.java
+++ src/test/java/com/ws/tests/BaseTest.java
@@ -2,14 +2,30 @@ package com.ws.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.ws.base.DriverScript;
 import com.ws.pages.HomePage;
 import com.ws.pages.LoginPage;
 
 public class BaseTest extends DriverScript {
 	
+	protected static ExtentHtmlReporter extent;
+	protected static ExtentReports report;
+	protected static ExtentTest logger;
 	HomePage homePage;
 	LoginPage loginPage;
+	
+	@BeforeSuite
+	public void setUpReport() 
+	{
+		extent = new ExtentHtmlReporter("./reports/index.html");
+		report = new ExtentReports();
+		report.attachReporter(extent);
+	}
 	
 	@BeforeMethod
 	public void setUp()
@@ -22,6 +38,7 @@ public class BaseTest extends DriverScript {
 	@AfterMethod
 	public void tearDown()
 	{
+		report.flush();
 		quitDriver();
 	}
 
